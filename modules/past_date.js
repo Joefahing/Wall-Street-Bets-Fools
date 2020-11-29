cc60f950ce68552dd4a77e57ac358a7611629b76
joefahing <cao11d@uwindsor.ca>
2020-11-29 13:47:51 -0500

    updated function to add summary to post and end point to get top post

--- modules/past_date.js
+++ modules/past_date.js
@@ -1,21 +1,22 @@
-class PastDate {
-    static pastDay(n_days = 1) {
-        const date = new Date();
+class PastTimestamp {
+
+    static pastDay(reference_date = new Date(), n_days = 1) {
+        const date = new Date(Date.parse(reference_date));
         date.setDate(date.getDate() - n_days);
         return date;
     }
 
-    static pastWeek(n_weeks = 1) {
-        const date = new Date();
+    static pastWeek(reference_date = new Date(), n_weeks = 1) {
+        const date = new Date(Date.parse(reference_date));
         date.setDate(date.getDate() - 7 * n_weeks);
         return date;
     }
 
-    static pastMonth(n_month = 1) {
-        const date = new Date();
+    static pastMonth(reference_date = new Date(), n_month = 1) {
+        const date = new Date(Date.parse(reference_date));
         date.setMonth(date.getMonth() - n_month, date.getDate());
         return date;
     }
 }
 
-module.exports = PastDate;
+module.exports = PastTimestamp;
--- routes/wsb_route.js
+++ routes/wsb_route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const wsb = require('../modules/wsb');
+const PastTimestamp = require('../modules/past_date');
+
 const router = express.Router();
 
 function invalidBodyPropertiesHandler(error, req, res, next) {
@@ -19,9 +21,76 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/gain_loss', async (req, res) => {
-    const gainLossPost = await wsb.getAllGainLossPost();
-    res.status(200).json(gainLossPost)
+router.get('/gain_loss/post', async (req, res, next) => {
+    const time_interval = req.query.interval;
+
+    if (!time_interval) next({
+        from: 'get_post',
+    })
+
+    else {
+        let current_interval_end_date = new Date();
+        let current_interval_start_date;
+        let previous_interval_end_date;
+        let previous_interval_start_date;
+        const pastBy = 1;
+
+
+        switch (time_interval) {
+            case 'day':
+                current_interval_start_date = PastTimestamp.pastDay();
+                previous_interval_end_date = PastTimestamp.pastDay(current_interval_end_date, pastBy);
+                previous_interval_start_date = PastTimestamp.pastDay(current_interval_start_date, pastBy);
+                break;
+
+            case 'week':
+                current_interval_start_date = PastTimestamp.pastWeek();
+                previous_interval_end_date = PastTimestamp.pastWeek(current_interval_end_date, pastBy);
+                previous_interval_start_date = PastTimestamp.pastWeek(current_interval_start_date, pastBy);
+                break;
+
+            case 'month':
+                current_interval_start_date = PastTimestamp.pastMonth();
+                previous_interval_end_date = PastTimestamp.pastMonth(current_interval_end_date, pastBy);
+                previous_interval_start_date = PastTimestamp.pastMonth(current_interval_start_date, pastBy);
+                break
+        }
+
+        const currentGainLossPosts = await wsb.getAllGainLossPost(current_interval_start_date, current_interval_end_date);
+
+        // const previousGainLossPosts = await wsb.getAllGainLossPost(
+        //     previous_interval_start_date, previous_interval_end_date);
+
+        const gainLossSummary = await wsb.getGainLossSummary(time_interval);
+
+        const result = {
+            summary: gainLossSummary,
+            date_used: {
+                interval: time_interval,
+                start_date: current_interval_start_date,
+                end_date: current_interval_end_date
+            },
+            posts: currentGainLossPosts,
+        }
+
+        console.log(result);
+
+        res.status(200).json(result)
+    }
+
+});
+
+router.get('/gain_loss/summary', async (req, res) => {
+    const summary = await wsb.getGainLossSummary('week');
+    console.log(summary);
+    res.status(200).json(summary);
+});
+
+router.get('/stock/top', async (req, res) => {
+    const result = await wsb.getTopNStocks();
+    res.json({
+        top_stock: result
+    });
 });
 
 router.post('/add_post', async (req, res, next) => {
