0e044459040dfba32b8eaf6ef93363ae0acdd034
joefahing <cao11d@uwindsor.ca>
2021-02-06 12:32:01 -0500

    updated util function

--- modules/utility.js
+++ modules/utility.js
@@ -1,10 +1,18 @@
-function trimTimeFromDate(date = new Date()) {
-    date.setSeconds(0, 0);
-    return date.getTime();
+function trimTimeFromDate(date = new Date(), setSecond = false) {
+
+    const indexOfMinute = 6
+    const isoString = date.toISOString();
+    const dateString = setSecond
+        ? isoString.substring(0, isoString.indexOf('T') + indexOfMinute) + ':00'
+        : isoString.substring(0, isoString.indexOf('T'));
+    const newDate = new Date(dateString);
+
+    return newDate.getTime();
 }
 
 function isInterval(interval = 'week') {
     const set = new Set(['week', 'day', 'month']);
+
     return set.has(interval);
 }
 
